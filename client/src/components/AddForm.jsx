27a10b25f8a5d4bd993d8bb81bd8d26f7c5244f5
Your Name <you@example.com>
2024-02-27 14:33:13 +0530

    second commit

--- client/src/components/AddForm.jsx
+++ client/src/components/AddForm.jsx
@@ -17,12 +17,19 @@ const[id, setId] = useState("")
  fetch('http://localhost:3000/post',{
   method:'POST',
   body:data
-  }).then(response=>{
-    response.json().then(data=>{
-     setId(data._id);
-     setRedirect(true)
-    })
-    })
+  }) .then(response => {
+    if (!response.ok) {
+      alert("Could not complete request! try again")
+    }
+    return response.json();
+  })
+  .then(data => {
+    setId(data._id);
+    setRedirect(true);
+  })
+  .catch(error => {
+    console.error('There was a problem with your fetch operation:', error);
+  });
 
 }
 if(redirect){
@@ -34,13 +41,21 @@ if(redirect){
     <div className='AddForm'>
      <form className='AddForm-Form' onSubmit={uploadForm}>
       <div className="heading">
-         <h1>Add pdf file to be extracted</h1>
+         <h1 className='text-5xl p-10'>
+          <span className='font-extrabold text-red-800'>PDF</span> Extracter</h1>
       </div>
+      <div className="desc">
+      Unlock the power of precision with our app! Seamlessly
+       extract pages from PDFs and craft custom documents with ease. Experience 
+      efficiency redefined as you create personalized PDFs tailored to your exact needs
+      </div>
+   
       <div className="file-input">
         <input  id="file-button"type="file" accept='.pdf' 
-        onChange={(e)=>setFile(e.target.files[0])} />
+        onChange={(e)=>setFile(e.target.files[0])} 
+        required/>
       </div>
-     <button>Upload PDF</button>
+     <button className='black-btn'>Upload PDF</button>
      </form>
     
     </div>
